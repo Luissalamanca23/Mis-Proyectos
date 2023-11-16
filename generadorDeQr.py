dd51d52af13fe1c1c9dcedd84c1ff16d5badbfa1
Luis Salamanca <luisslamanca@MacBook-M2-de-Lucho.local>
2023-11-16 09:59:44 -0300

    arreglo

--- generadorDeQr.py
+++ generadorDeQr.py
@@ -1,10 +1,10 @@
 import pyqrcode, png  # Copie "pip install pyqrcode pypng" y pegue en la terminal para instalar las librerias
 
-link = "https://www."  # Ingrese el link que desea convertir en QR "El link tiene que estar dentro de las comillas"
+link = "https://github.com/Luissalamanca23/"  # Ingrese el link que desea convertir en QR "El link tiene que estar dentro de las comillas"
 qr_code = pyqrcode.create(link)
 qr_code.png(
-    "----nombre----.png",  # Ingrese le nombre con el cual desea guardar el QR 
+    "GitHub de Luis Salamanca.png",  # Ingrese le nombre con el cual desea guardar el QR 
                            # "El nombre tiene que estar dentro de las comillas y al final con .png"
-    scale=8,  # Tamaño del del png o resolucion.
+    scale=20,  # Tamaño del del png o resolucion.
 )
 print("QR generado con éxito")
